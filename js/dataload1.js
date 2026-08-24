c8719f66f665e7c6614c2e4dd25a68d7d01faa75
trium1975-cloud <trium1975@gmail.com>
2026-08-24 17:46:05 +0900

    Add files via upload

---
+++ js/dataload1.js
@@ -0,0 +1,18 @@
+ fetch("./json/01_banchan.json")
+      .then(res => res.json())
+      .then(data => {
+        const box = document.querySelector(".banchan");
+
+        data.forEach(item => {
+          box.innerHTML += `
+            <a href="#">
+              <div class="product-card">
+                <img src="${item.main_img}" alt="${item.name}">
+                <h4>${item.name}</h4>
+                <p>${item.description}</p>
+                <h5>${item.price}원</h5>
+              </div>
+            </a>
+          `;
+        });
+      });
